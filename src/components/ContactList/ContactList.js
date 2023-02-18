0c22d3669ba143368d34957585454af78139b240
Katya-Semeniuk <katyasemeniuk@gmail.com>
2023-02-18 21:12:37 +0100

    for check

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './ContactList.css';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 
 const ContactList = ({ arrayContact, onDeleteContact }) => {
@@ -13,8 +13,7 @@ const ContactList = ({ arrayContact, onDeleteContact }) => {
               <p className="name">{name}: </p>
                      <p className="tel">{number}</p>
                      <button onClick={()=> onDeleteContact(id)} className="btn_delete">Delete</button>  
-            </div>
-              
+            </div>   
           </li>
          ))}
            
